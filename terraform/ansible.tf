9d627b3a47f1c58b0447289ebb13948a76397f6a
pezhore <pezhore@gmail.com>
2023-02-07 11:13:00 -0600

    Adding fqdn and reformatting inventory file

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -8,6 +8,7 @@ resource "local_file" "ansible_inventory" {
   content = templatefile("external/inventory.tmpl",
     {
       lab_vms = proxmox_vm_qemu.core_vms
+      fqdn    = local.config.lab.fqdn
     }
   )
   filename = "../ansible/lab-inventory.ini"
