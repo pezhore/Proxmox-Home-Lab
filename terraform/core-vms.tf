0bf9b5b1ea9644871f7ccee1864e8a699fc27de4
pezhore <pezhore@gmail.com>
2023-02-07 09:58:27 -0600

    moving to dynamic blocks for disk and network, adding comments

--- terraform/core-vms.tf
+++ terraform/core-vms.tf
@@ -6,49 +6,68 @@
 # automatically provision/deprovision VMs based on the contents of that file.
 
 resource "proxmox_vm_qemu" "core_vms" {
+
+  # Iterate over our yaml config with each core_vm has its own config
   for_each    = local.config.core_vms
   name        = each.key
+  desc        = each.value.description
+
   target_node = local.config.lab.node
-  # another variable with contents "ubuntu-2004-cloudinit-template"
   clone = each.value.template
-  # basic VM settings here. agent refers to guest agent
+
+  # When set to 1, the QEMU Guest Agent is enabled. This does require
+  # installing/running the qemu-guest-agent service inside the VM
   agent   = 1
-  os_type = "cloud-init"
+  os_type = each.value.os_type
   cores   = each.value.cores
   sockets = each.value.sockets
-  cpu     = "host"
+  cpu     = "host"              # Set the type of CPU to whatever our proxmox host has
   memory  = each.value.memory
-  scsihw  = "virtio-scsi-pci"
+  scsihw  = each.value.scsihw
 
-  disk {
-    slot = 0
-    # set disk size here. leave it small for testing because expanding the disk takes time.
-    size     = each.value.disk_size
-    type     = "scsi"
-    storage  = local.config.lab.storage
-    format   = "qcow2"
+  # Iterate over our disks and create a block for each one
+  # This allows for multiple disks to be specified in our config without having
+  # to edit our terraform code
+  dynamic "disk" {
+    iterator = disk
+    for_each = each.value.dynamic.disks
+
+    content {
+      size    = disk.value.size
+      type    = disk.value.type
+      storage = disk.value.storage
+      format  = disk.value.format
+    }
   }
 
-  # if you want two NICs, just copy this whole network section and duplicate it
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
+  # Iterate over our networks and create a block for each one
+  # This allows for multiple network configs to be specified in our config without having
+  # to edit our terraform code
+  dynamic "network" {
+    iterator = nic
+    for_each = each.value.dynamic.networks
+    content {
+      model  = nic.value.model
+      bridge = nic.value.bridge
+    }
   }
-  # not sure exactly what this is for. presumably something about MAC addresses and ignore network changes during the life of the VM
+
+  # I'm not sure if this is needed, possibly because the default behavior if a
+  # network change occurs is to reprovision the system?
   lifecycle {
     ignore_changes = [
       network,
     ]
   }
 
-  # the ${count.index + 1} thing appends text to the end of the ip address
-  # in this case, since we are only adding a single VM, the IP will
-  # be 10.98.1.91 since count.index starts at 0. this is how you can create
-  # multiple VMs and have an IP assigned to each (.91, .92, .93, etc.)
+
+  # Annoyingly, there doesn't appear to be a way to dynamically create more
+  # default IP addresses for each network interface - so we crete the first one,
+  # then use Ansible to configure/set the rest.
   ipconfig0 = "ip=${each.value.ipaddr},gw=${each.value.gateway}"
 
   # sshkeys set using variables. the variable contains the text of the key.
   sshkeys = <<EOF
-  ${local.config.lab.sshkey}
+  ${data.http.github_ssh_keys.response_body}
   EOF
 }
