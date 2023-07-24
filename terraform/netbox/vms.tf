de81c48511b018f1b70629fec924e717064bdfe3
pezhore <pezhore@gmail.com>
2023-07-24 09:27:39 -0500

    Bringing in extra DNS entries

--- terraform/netbox/vms.tf
+++ terraform/netbox/vms.tf
@@ -20,6 +20,7 @@ resource "netbox_ip_address" "core_vms_ip" {
 
   ip_address   = "${each.value.ipv4}/24"
   status       = "active"
+  dns_name     = "${each.key}.lan.pezlab.dev"
   interface_id = netbox_interface.core_vms_eth0[each.key].id
 }
 
@@ -28,4 +29,12 @@ resource "netbox_primary_ip" "myvm_primary_ip" {
 
   ip_address_id      = netbox_ip_address.core_vms_ip[each.key].id
   virtual_machine_id = netbox_virtual_machine.core_vms[each.key].id
+}
+
+resource "netbox_ip_address" "extra_dns_ip" {
+  for_each = local.config.extra_dns
+
+  ip_address = "${each.value.ipv4}/24"
+  status = "active"
+  dns_name = "${each.key}.lan.pezlab.dev"
 }
