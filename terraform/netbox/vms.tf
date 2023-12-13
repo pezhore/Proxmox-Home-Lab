492a7fb9deb54f44a32ee0668fae6bb1c793e8fa
pezhore <pezhore@gmail.com>
2023-12-13 16:52:30 -0600

    bringing netbox in line with broken out config files

--- terraform/netbox/vms.tf
+++ terraform/netbox/vms.tf
@@ -1,13 +1,13 @@
 resource "netbox_virtual_machine" "core_vms" {
-  for_each   = local.config.core_vms
+  for_each   = local.vms
   cluster_id = netbox_cluster.pve.id
   name       = each.key
   memory_mb  = each.value.memory
-  vcpus      = each.value.vcpus
+  vcpus      = try(each.value.vcpus, each.value.cores)
 }
 
 resource "netbox_interface" "core_vms_eth0" {
-  for_each = local.config.core_vms
+  for_each = local.vms
 
   name               = "eth1"
   enabled            = true
@@ -16,23 +16,23 @@ resource "netbox_interface" "core_vms_eth0" {
 
 
 resource "netbox_ip_address" "core_vms_ip" {
-  for_each = local.config.core_vms
+  for_each = local.vms
 
-  ip_address   = "${each.value.ipv4}/24"
+  ip_address   = "${each.value.dynamic.networks["primary"].ip}/24"
   status       = "active"
   dns_name     = "${each.key}.lan.pezlab.dev"
   interface_id = netbox_interface.core_vms_eth0[each.key].id
 }
 
 resource "netbox_primary_ip" "myvm_primary_ip" {
-  for_each = local.config.core_vms
+  for_each = local.vms
 
   ip_address_id      = netbox_ip_address.core_vms_ip[each.key].id
   virtual_machine_id = netbox_virtual_machine.core_vms[each.key].id
 }
 
 resource "netbox_ip_address" "extra_dns_ip" {
-  for_each = local.config.extra_dns
+  for_each = local.extradns
 
   ip_address = "${each.value.ipv4}/24"
   status = "active"
