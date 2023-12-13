43e09f227aff2fe86d8c67357ca53cbdb66c62e8
pezhore <pezhore@gmail.com>
2023-12-13 16:53:54 -0600

    Brining in ansible inventory generation .... but it needs more thought

---
+++ terraform/core/ansible.tf
@@ -0,0 +1,16 @@
+#################################################
+# ansible.tf
+#
+# This file contains the resources to generate a dynamic inventory file for
+# Ansible.
+
+# resource "local_file" "ansible_inventory" {
+#   content = templatefile("../external/inventory.tmpl",
+#     {
+#       groups  = local.config.ansible_groups
+#       lab_vms = local.config.core_vms
+#       fqdn    = local.config.lab.fqdn
+#     }
+#   )
+#   filename = "../../ansible/lab-inventory.ini"
+# }
