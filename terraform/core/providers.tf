f52f501abb7fd4c756a2db9b062f94687c613a80
Brian Marsh <pezhore@gmail.com>
2023-12-23 19:18:09 -0600

    removing debug and formatting

--- terraform/core/providers.tf
+++ terraform/core/providers.tf
@@ -20,13 +20,6 @@ provider "telmate" {
   pm_api_url  = "https://pve-01.lan.pezlab.dev:8006/api2/json"
   pm_user     = "root@pam"
   pm_password = data.vault_generic_secret.shared.data["root_password"]
- pm_log_enable = true
-  pm_log_file = "terraform-plugin-proxmox.log"
-  pm_debug = true
-  pm_log_levels = {
-    _default = "debug"
-    _capturelog = ""
- }
 }
 
 provider "bpg" {
